e766d685b10a28c0f3cd2de3e3b92abb5f904f53
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-19 08:28:23 +0300

    update App component to class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import { ViewItem } from './component/ViewItem/ViewItem';
 import AddItem from './component/AddItem/AddItem';
-function App() {
-  return (
-    <section className="App">
-      <h2>Simple Crud App</h2>
-      <AddItem />
-      <ViewItem />
-    </section>
-  );
-}
 
-export default App;
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+  }
+  
+  render() {
+    return (
+      <section className="App">
+        <h2>Simple Crud App</h2>
+        <AddItem />
+        <ViewItem />
+      </section>
+    )
+  }
+}
